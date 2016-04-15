d0785391feebbb6b76271d2e693abf9ff17818d6
Chethan Bhat <chethanbhat@gmail.com>
2016-04-15 12:03:14 +0530

    Finished form_tag. form_for and editing remains

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+
+    validates :username, presence: true,  length: {minimum: 5}
+    validates :email, presence: true,  length: {minimum: 5}
+    validates :password, presence: true,  length: {minimum: 5}
+
+
+end
