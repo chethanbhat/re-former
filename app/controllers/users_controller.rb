1beea52694787c14c80f61f5d5d7a38a26cb9776
Chethan Bhat <chethanbhat@gmail.com>
2016-04-16 16:58:00 +0530

    form_for, user edit feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
 
     def new
+
+      @user = User.new
+
     end
 
     def create
 
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-      
-        #@user = User.new(user_params)
+      #  @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+        @user = User.new(user_params)
 
         if @user.save
           redirect_to new_user_path
@@ -15,6 +18,25 @@ class UsersController < ApplicationController
           render :new
         end
     end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+
+      #  @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+          render :edit
+        else
+          render :edit
+        end
+    end
+
+
 
     private
 
